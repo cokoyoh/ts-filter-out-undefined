8ce972871c1591b84ca38a34636f5cb233393b4e
cokoyoh <charlesokoyoh@gmail.com>
2020-09-06 10:06:05 +0300

    Filter out undefined using the && short-hand

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Filter all of the undefineds out of the array.
+ * You can define an isNotUndefined function to use with filter, or you can write an arrow function maybeUndefined => { ... }
+ */
+var maybeNumbers = [1, 2, undefined, 3, 4];
+var isNotUndefined = function (element) {
+    return element !== undefined && element;
+};
+var numbers = maybeNumbers.filter(function (element) { return isNotUndefined(element); });
+console.log(numbers);
---
+++ index.ts
@@ -0,0 +1,14 @@
+/**
+ * Filter all of the undefineds out of the array. 
+ * You can define an isNotUndefined function to use with filter, or you can write an arrow function maybeUndefined => { ... }
+ */
+
+const maybeNumbers: (number | undefined)[] = [1, 2, undefined, 3, 4];
+
+const isNotUndefined = (element: number | undefined) => {
+    return  element !== undefined && element; 
+}
+
+const numbers: number[] = maybeNumbers.filter((element) => isNotUndefined(element));
+
+console.log(numbers);
